66384022d50bc75844b691c272ff1c86279a915f
scarylp666 <scarylpsk@gmail.com>
2024-02-09 18:49:23 +0100

    jebohlava

--- main/js/output.js
+++ main/js/output.js
@@ -1,21 +1,21 @@
-function get_weather_data() {
-	const xhttp = new XMLHttpRequest();
-	xhttp.onload = function () {
-		weather_data = JSON.parse(this.responseText);
-		document.getElementById("temperature").innerHTML =
-			weather_data["Temperature"];
-		document.getElementById("humidity").innerHTML = weather_data["Humidity"];
-		document.getElementById("timestamp").innerHTML = weather_data["Time"];
-		document.getElementById("temperature1").innerHTML =
-			weather_data["Temperature1"];
-		document.getElementById("humidity1").innerHTML = weather_data["Humidity1"];
-		document.getElementById("timestamp1").innerHTML = weather_data["Time1"];
-		document.getElementById("Rain").innerHTML = weather_data["Rain"];
-		document.getElementById("Speed").innerHTML = weather_data["Speed"];
-		document.getElementById("pressure").innerHTML = weather_data["pressure"];
-	};
-	xhttp.open("GET", "main/weather_data.json");
-	xhttp.send();
+// function get_weather_data() {
+	// const xhttp = new XMLHttpRequest();
+	// xhttp.onload = function () {
+	// 	weather_data = JSON.parse(this.responseText);
+	// 	document.getElementById("temperature").innerHTML =
+	// 		weather_data["Temperature"];
+	// 	document.getElementById("humidity").innerHTML = weather_data["Humidity"];
+	// 	document.getElementById("timestamp").innerHTML = weather_data["Time"];
+	// 	document.getElementById("temperature1").innerHTML =
+	// 		weather_data["Temperature1"];
+	// 	document.getElementById("humidity1").innerHTML = weather_data["Humidity1"];
+	// 	document.getElementById("timestamp1").innerHTML = weather_data["Time1"];
+	// 	document.getElementById("Rain").innerHTML = weather_data["Rain"];
+	// 	document.getElementById("Speed").innerHTML = weather_data["Speed"];
+	// 	document.getElementById("pressure").innerHTML = weather_data["pressure"];
+	// };
+	// xhttp.open("GET", "main/weather_data.json");
+	// xhttp.send();
 }
 // get_weather_data();
 // setInterval(get_weather_data, 1000);
